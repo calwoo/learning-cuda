85370be967683c4aff2c941697d0d1fc7ecaa95b
calvin d. woo <calvin.d.woo@gmail.com>
2023-10-28 22:46:29 -0400

    finish up to k&r 1.4

--- kr-refresh/ch1/exercise-1-3.c
+++ kr-refresh/ch1/exercise-1-3.c
@@ -2,20 +2,20 @@
 
 main()
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;   /* lower limit of temperature table */
     upper = 300; /* upper limit */
     step = 20;   /* step size */
 
-    printf("%s\t%s\n", "FAHR", "CELSIUS");
+    printf("%s %s\n", "FAHR", "CELSIUS");
   	printf("---------------\n");
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }

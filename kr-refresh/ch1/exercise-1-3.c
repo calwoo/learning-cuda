3b285b5a7a76364b34276b55b6d58e9a404346ef
calvin d. woo <calvin.d.woo@gmail.com>
2023-10-28 22:27:07 -0400

    start reviewing k&r

---
+++ kr-refresh/ch1/exercise-1-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+main()
+{
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;   /* lower limit of temperature table */
+    upper = 300; /* upper limit */
+    step = 20;   /* step size */
+
+    printf("%s\t%s\n", "FAHR", "CELSIUS");
+  	printf("---------------\n");
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr-32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}

55db57b8435a6b8aaeb382a9f99fa493369615de
calvin d. woo <calvin.d.woo@gmail.com>
2023-10-29 01:57:42 -0400

    finish k&r ch 1

---
+++ kr-refresh/ch1/exercise-1-19.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+void reverse(char s[]);
+
+main()
+{
+    char test[MAXLINE] = "hello world";
+    reverse(test);
+
+    printf("%s\n", test);
+}
+
+void reverse(char s[])
+{
+    char tmp;
+
+    // get length of string
+    int i = 0;
+    while (s[i] != '\0')
+        i++;
+    
+    // reverse string
+    i--;
+    int j = 0;
+    while (j < i) {
+        tmp = s[j];
+        s[j] = s[i];
+        s[i] = tmp;
+        i--;
+        j++;
+    }
+}
--- sheffield-com4521/lab1/exercise1.c
+++ sheffield-com4521/lab1/exercise1.c
@@ -1,7 +1,14 @@
 /* Hello World program */
 #include <stdio.h>
+#include "random.h"
+
+#define NUM_VALUES 250
+
 int main()
 {
+    
+
+
     // output some text
     printf("Hello World\n");
     return 0;
